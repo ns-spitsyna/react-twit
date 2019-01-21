f61c6eac828a066b280b99b5cc3badcf7ddfd2e4
ns-spitsyna <spitcinan@gmail.com>
2019-01-22 00:19:55 +0700

    Урок 3.React

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () =>{
+    return (
+        <div className="app-header d-flex">
+        <h1>Nadezhda Spitsyna</h1>
+        <h2>7 записей, из них прочитано 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
